8fe98391454d49121cd72a1aa8a4834c9d488623
jenamndz <jenamndz18@gmail.com>
2026-04-28 01:41:54 +0800

    sorts the numbers in the txt file, puts the even numbers to even.txt then the odd numbers to odd.txt

--- even_odd_file_extractor.py
+++ even_odd_file_extractor.py
@@ -1,3 +1,14 @@
 try:
     with open("numbers.txt", "r") as input_file:
         with open("even.txt", "w") as even_file, open("odd.txt", "w") as odd_file:
+
+    for line in input_file:
+        num_str = line.strip()
+
+        if num_str:
+            number = int(num_str)
+
+            if number % 2 == 0:
+                even_file.write(str(number) + "\n")
+            else:
+                odd_file.write(str(number) + "\n")
