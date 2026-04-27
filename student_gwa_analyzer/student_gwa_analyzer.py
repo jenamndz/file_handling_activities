f5d6de41297be3341729c44f833ee3c787f9536a
jenamndz <jenamndz18@gmail.com>
2026-04-28 02:34:28 +0800

    determines who has the highest gwa and stores it

--- student_gwa_analyzer/student_gwa_analyzer.py
+++ student_gwa_analyzer/student_gwa_analyzer.py
@@ -6,3 +6,11 @@ try:
     with open("students_gwa.txt", "r") as file:
         for line in file:
             parts = line.strip().split(",")
+
+            if len(parts) == 2:
+                name = parts[0]
+                gwa = float(parts[1])
+
+                if gwa > highest_gwa:
+                    highest_gwa = gwa
+                    top_student = name
