361a910b93490663edc6e2949926e0ec45647f17
jenamndz <jenamndz18@gmail.com>
2026-04-28 22:50:14 +0800

    reads the file line by line

--- even_square_odd_cube_sorter/even_square_odd_cube_sorter.py
+++ even_square_odd_cube_sorter/even_square_odd_cube_sorter.py
@@ -2,3 +2,8 @@ def process_integers():
     try:
         with open("integers.txt", "r") as input_file:
             with open("double.txt", "w") as double_file, open("triple.txt", "w") as triple_file:
+
+                for line in input_file:
+                    num_str = line.strip()
+                    if num_str:
+                        number = int(num_str)
